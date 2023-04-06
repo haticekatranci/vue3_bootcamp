79d56a1e352c529fc6e0c9c9056b7a66d01a27d3
hatice <hatice@hotmail.com>
2023-04-07 01:30:33 +0300

    class style binding

--- 6-class-style-binding/app.js
+++ 6-class-style-binding/app.js
@@ -1,50 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            title: "Test Başlığı",
-            itemList:[],
-        }
-
+            showBorder: false,
+            redBG: false,
+            boxClass: "border red",
+            bgColor: "cyan",
+        };
     },
-    beforeCreate() {
-        console.log("beforeCreate Çalıştı..")
+    computed: {
+        boxClasses() {
+            return { border: this.showBorder, red: this.redBG };
+        },
     },
-    created() {
-        console.log("created Çalıştı..");
-        setTimeout(() =>{
-            this.itemList = [1,2,3,4,5,6,7,8,9]
-
-        },2000);
-
-        setTimeout(() => {
-            this.title = "Bu yeni başlık"
-
-        },3000)
-    },
-    beforeMount() {
-        console.log("beforeMount Çalıştı..")
-    },
-    mounted() {
-        console.log("mounted Çalıştı..")
-    },
-    beforeUpdate (){
-        console.log("beforeUpdate Çalıştı..")
-    },
-    updated() {
-        console.log("updated Çalıştı..")
-    },
-    beforeUnmount() {
-        console.log("beforeUnmount Çalıştı..")
-    },
-    unmounted() {
-        console.log("unmounted Çalıştı..")
-    },
-
-
-
-});
-app.mount("#app");
-
-setTimeout(()=>{
-    app.unmount();
-}, 5000);
+}).mount("#app");
